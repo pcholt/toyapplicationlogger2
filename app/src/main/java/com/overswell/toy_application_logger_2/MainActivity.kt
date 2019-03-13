f4aabbea59cf487d0d6f8d1d0870c1389cad8175
ph <pcholt@gmail.com>
2019-03-13 22:30:56 +1100

    Initial commit

---
+++ app/src/main/java/com/overswell/toy_application_logger_2/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.overswell.toy_application_logger_2
+
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.get
+
+class MainActivity : AppCompatActivity() {
+
+    private val fusedLocationClient by lazy { LocationServices.getFusedLocationProviderClient(this) }
+    val vm : MainViewModel = get()
+    private val myLocationPermission = "locationPermission"
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        log_location.setOnClickListener {
+            if (!isPermissionGranted()) {
+                requestPermissions()
+            } else {
+                try {
+                    fusedLocationClient.lastLocation.addOnSuccessListener {
+                        if (it != null) {
+                            vm.locationUpdate(it)
+                            textView.text = "Alt: ${it.altitude}, Long: ${it.longitude}, Lat: ${it.latitude}"
+                        }
+                    }
+                } catch (e: SecurityException) {
+
+                }
+            }
+        }
+    }
+
+    private fun requestPermissions() =
+        ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION, myLocationPermission), 1)
+
+    private fun isPermissionGranted() =
+        checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED
+
+}
+
+class MainViewModel(
+    val db: DatabaseReference,
+    val fusedLocationProviderClient: FusedLocationProviderClient
+) : ViewModel() {
+
+    fun locationUpdate(location: Location) {
+        with(location) {
+            db.child("visit").push().setValue(LocationRecord(altitude,longitude,latitude))
+        }
+    }
+
+}
+
+data class LocationRecord (
+    val altitude: Double,
+    val longitude: Double,
+    val latitude: Double
+)
---
+++ app/src/main/java/com/overswell/toy_application_logger_2/MainApplication.kt
@@ -0,0 +1,28 @@
+package com.overswell.toy_application_logger_2
+
+import android.app.Application
+import com.google.android.gms.location.LocationServices
+import com.google.firebase.database.FirebaseDatabase
+import org.koin.android.ext.android.startKoin
+import org.koin.androidx.viewmodel.ext.koin.viewModel
+import org.koin.dsl.module.module
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(this, listOf(
+            firebaseModule,
+            viewModelModule
+        ))
+    }
+}
+
+val firebaseModule = module {
+    single { FirebaseDatabase.getInstance() }
+    single { get<FirebaseDatabase>().reference }
+    single { LocationServices.getFusedLocationProviderClient(this) }
+}
+
+val viewModelModule = module {
+    viewModel { MainViewModel() }
+}
